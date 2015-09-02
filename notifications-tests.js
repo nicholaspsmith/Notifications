aa1deb30b0f65a23238263b05681a43242327a04
Nick P Smith <npsmith1990@gmail.com>
2015-09-02 12:58:16 -0600

    remove test that is not applicable

--- notifications-tests.js
+++ notifications-tests.js
@@ -6,16 +6,3 @@ Tinytest.add("Notifications - collection", function(test) {
 
   Notifications.collection.remove({});
 });
-
-Tinytest.addAsync("Notifications - template", function(test, done) {
-  Notifications.push('A new notification!');
-  test.equal(Notifications.collection.find({}).count(), 1);
-
-  // render the template
-  UI.insert(UI.render(Template.easyNotifications), document.body);
-
-  Meteor.setTimeout(function() {
-    test.equal(Notifications.collection.find({}).count(), 0);
-    done();
-  }, 3500);
-});
